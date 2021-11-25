6908257c9a79be4faede192182fd47c0dc5ee8a7
Kemal Abdillah <60648376+kemal003@users.noreply.github.com>
2021-11-25 21:45:19 +0700

    Update login + firebase

--- app/src/main/java/com/example/park_tikom/Login.kt
+++ app/src/main/java/com/example/park_tikom/Login.kt
@@ -2,10 +2,28 @@ package com.example.park_tikom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 
 class Login : AppCompatActivity() {
+    private var doubleBackExit = false;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    override fun onBackPressed() {
+        if (doubleBackExit){
+            super.onBackPressed()
+            return;
+        }
+
+        this.doubleBackExit = true;
+        Toast.makeText(this, "Tekan sekali lagi untuk keluar", Toast.LENGTH_SHORT).show()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            doubleBackExit = false }, 1000)
     }
 }
--- app/src/main/java/com/example/park_tikom/MainActivity.kt
+++ app/src/main/java/com/example/park_tikom/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.park_tikom
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        println("Ini testing first commit")
+//        setContentView(R.layout.activity_main)
+        val login = Intent(this, Login::class.java)
+        startActivity(login)
     }
 }
