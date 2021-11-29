35480f1020c93c4c95a644621eee232500f7226f
Kautsarratu <64664923+Kautsarratu@users.noreply.github.com>
2021-11-29 13:18:42 +0700

    update pesanan dan profil

---
+++ app/src/main/java/com/example/park_tikom/DetailPesananActivity.kt
@@ -0,0 +1,11 @@
+package com.example.park_tikom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailPesananActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_pesanan)
+    }
+}
---
+++ app/src/main/java/com/example/park_tikom/ProfilActivity.kt
@@ -0,0 +1,11 @@
+package com.example.park_tikom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
